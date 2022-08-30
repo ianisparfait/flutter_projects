c823bb8eaa6459bdf5b56565f601e6a5f1d44090
Ianis <ianis.parfait@marklix.com>
2022-08-30 14:35:28 +0200

    init new project + update name's

---
+++ boucle_view/android/app/src/main/kotlin/com/example/boucle_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boucle_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- boucle_view/lib/main.dart
+++ boucle_view/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Increment number, conc string n\' invers var with other function',
+      title: 'Boucle n\' list in flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -49,9 +49,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  static const _text = "salut";
-  static const _arr = [1, 2, "salut"];
-  bool _s = true;
 
   void _incrementCounter() {
     setState(() {
@@ -61,12 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-    });
-  }
-
-  void _changeString() {
-    setState(() {
-      _s = !_s;
     });
   }
 
@@ -105,19 +96,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-                'You have pushed the button this many times, but before that: $_text:',
-                textAlign: TextAlign.center),
+              'You have pushed the button this many times:',
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text('coucou, the first index in array {_arr} is ${_arr[0]}'),
-            Text('Boolean: $_s')
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _changeString,
+        onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
